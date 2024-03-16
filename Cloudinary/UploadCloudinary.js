b8baae2ad41e65e3a4b31658353e15f8de7282f0
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-16 20:57:01 +0600

    Cloudinary

---
+++ Cloudinary/UploadCloudinary.js
@@ -0,0 +1,7 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dxvyxea4n', 
+  api_key: '947956513668134', 
+  api_secret: 'hYWME4Hw69T94-mjq6gtBeqxF4U' 
+});
