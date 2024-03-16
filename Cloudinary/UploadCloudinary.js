2462501cdb8d6ee8735cf5631ed1db5ed76527e2
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-16 21:23:58 +0600

    Upload Img Cloudinary

--- Cloudinary/UploadCloudinary.js
+++ Cloudinary/UploadCloudinary.js
@@ -1,7 +1,18 @@
-import {v2 as cloudinary} from 'cloudinary';
+const cloudinary = require("cloudinary");
           
 cloudinary.config({ 
   cloud_name: 'dxvyxea4n', 
   api_key: '947956513668134', 
   api_secret: 'hYWME4Hw69T94-mjq6gtBeqxF4U' 
 });
+
+const UploadCloudinary = async (localpath)=>{
+   try {
+   const UploadImg =  await cloudinary.uploader.upload(localpath)
+   return UploadImg
+   } catch (error) {
+    console.log("UploadCloudinary catch error" ,error);
+   }
+} 
+
+module.exports = UploadCloudinary
--- MulterController.js
+++ MulterController.js
@@ -1,4 +1,9 @@
-function MulterController (req,res){
-// console.log(req);
+const UploadCloudinary = require("./Cloudinary/UploadCloudinary");
+
+async function MulterController (req,res){
+ 
+ const {path} = req.file
+ const resCloudinary = await UploadCloudinary(path)
+ console.log(resCloudinary.url);
 }
 module.exports = MulterController;
